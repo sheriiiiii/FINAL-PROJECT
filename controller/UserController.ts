283ea67183aa5e23710d34454737b9d3324ca4d5
AllanLoydPacete <allanloydpacete@gmail.com>
2024-05-18 18:07:09 +0800

    initialized backend

---
+++ controller/UserController.ts
@@ -0,0 +1,82 @@
+import express from 'express';
+import UserService from "../service/UserService";
+
+class UserController {
+
+    async loginUser(req: express.Request, res: express.Response) {
+        try {
+            const user = UserService.login(req.body);
+            if ("error" in user) {
+                return user.error;
+            } else {
+                return user
+            }
+        } catch (error) {
+            console.log("error in login");
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+
+    async registerUser(req: express.Request, res: express.Response) {
+        try {
+            const user = UserService.register(req.body);
+            if ("error" in user) {
+                return user.error;
+            } else {
+                return user
+            }
+        } catch (error) {
+            console.log("error in login");
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+
+    async createUser(req:express.Request, res: express.Response) {
+        try {
+            await UserService.createUser(req.body)
+            return res.status(200).json({message: "User has been created successfully"});
+        } catch (error) {
+            console.log("error in createUser: ", error)
+            return res.status(500).send({error: "Internal Server Error."});
+        }
+    }
+
+    async getUserByEmail(req: express.Request, res: express.Response) {
+        const { email } = req.body;
+        try {
+            const response = await UserService.getUserByEmail(email);
+            if (response === null) {
+                return res.status(404).json({ error: "user not found" });
+            } else {
+                return res.status(201).json(response)
+            }
+        } catch (error) {
+            console.log("error in getUserByEmail: ", error)
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+
+    async editUserInfo(req: express.Request, res: express.Response) {
+        const user_id = req.params.id;
+        try {
+            const response = await UserService.editUserInfo(req.body, user_id);
+            return res.status(201).json(response);
+        } catch (error) {
+            console.log("error in editUserInfo: ", error);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+
+    async deleteUser(req: express.Request, res: express.Response) {
+        const { user_id }= req.body
+        try {
+            await UserService.deleteUser(user_id);
+            return res.status(200).json({ message: "user successfully deleted" });
+        } catch (error) {
+            console.log("error in deleteUser: ", error)
+            return res.status(500).json({ error: "Internal Server Error" })
+        }
+    }
+}
+
+export default new UserController();
---
+++ service/UserService.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+interface IUserInfo {
+    fname: string,
+    lname: string,
+    email: string,
+    password: string,
+    type: string
+}
+
+class UserService {
+
+    // feel ko ma sala ni
+    async createUser(data: IUserInfo) {
+        return await prisma.user.create({
+            data,
+        })
+    }
+
+    async getUserByEmail(email: string) {
+        return await prisma.user.findUnique({
+            where: { email }
+        })
+    }
+
+    async editUserInfo(data: IUserInfo, user_id: string) {
+        return await prisma.user.update({
+            where: {id: user_id},
+            data
+        })
+    }
+
+    async deleteUser(user_id: string) {
+        return await prisma.user.delete({
+            where: { id: user_id }
+        })
+    }
+
+    async login(data: IUserInfo) {
+        try {
+            const user = await this.getUserByEmail(data.email);
+            if (user === null) return { message: "no user found" };
+            else if (user.password !== data.password) return { message: "wrong password. try again" };
+            else return user;
+        } catch (error) {
+            console.log("error logging in");
+            return { error: "Error logging in" };
+        }
+    }
+
+    async register(data: IUserInfo) {
+        try {
+            const user = await this.createUser(data);
+            return user
+        } catch (error) {
+            console.error("register error: ", error);
+            return { error: "error in registering new user" };
+        }
+    }
+}
+
+export default new UserService();
